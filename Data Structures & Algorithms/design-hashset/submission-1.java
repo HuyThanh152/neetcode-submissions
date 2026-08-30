e8d244f844fd9c72115cf33004d4d45d02190636
HuyThanh152 <thanh.nh2516259@sis.hust.edu.vn>
2026-08-30 09:46:56 +0700

    Add: design-hashset - submission-1

---
+++ Data Structures & Algorithms/design-hashset/submission-1.java
@@ -0,0 +1,65 @@
+class MyHashSet {
+    class Node {
+        int val;
+        Node next;
+        Node(int val) { this.val = val; }
+    }
+    private int size;
+    private Node[] buckets;
+    public MyHashSet() {
+        size = 1000;
+        buckets = new Node[size];
+    }
+    private int hash(int key) {
+        return key % size;
+    }
+    public void add(int key) {
+        int i = hash(key);
+        if(buckets[i]==null) {
+            buckets[i] = new Node(key);
+            return;
+        }
+        Node curr = buckets[i];
+        while(true) {
+            if(curr.val == key) return;
+            if (curr.next == null) break;
+            curr = curr.next;
+        }
+        
+        curr.next = new Node(key);
+    }
+    
+    public void remove(int key) {
+        int i = hash(key);
+        Node curr = buckets[i];
+        Node prev = null;
+
+        while( curr != null) {
+            if(curr.val == key) {
+                if(prev == null) buckets[i] = curr.next;
+                else prev.next = curr.next;
+                return;
+            }
+            prev = curr;
+            curr = curr.next;
+        }
+    }
+    
+    public boolean contains(int key) {
+        int i = hash(key);
+        Node curr = buckets[i];
+        while(curr != null) {
+            if(curr.val == key) return true;
+            curr = curr.next;
+        }
+        return false;
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
